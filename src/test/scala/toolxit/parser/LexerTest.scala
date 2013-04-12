679a517c42ce248bc599f13418d7f26f46d08bc9
Lucas Satabin <lucas.satabin@gnieh.org>
2013-04-12 18:44:15 +0200

    add some lexer tests

---
+++ src/test/scala/toolxit/parser/LexerTest.scala
@@ -0,0 +1,108 @@
+/*
+* This file is part of the ToolXiT project.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package toolxit
+package parser
+
+import util._
+
+import org.scalatest._
+
+class LexerTest extends FlatSpec with ShouldMatchers {
+
+  val lexer = new TeXLexers[StreamPosition[Char]] with StreamProcessor[Char, StreamPosition[Char], Token] {
+
+    val transformer = token
+
+    protected def nextPos(current: StreamPosition[Char], read: Char): StreamPosition[Char] =
+      current.next
+  }
+
+  def stringOf(token: Token) =
+    token.toString + token.pos
+
+  import lexer._
+
+  val env = new TeXEnvironment(None)
+
+  val basicCategories = Map(
+    '^' -> Category.SUPERSCRIPT
+  )
+
+  def withCat(cats: Map[Char, Category.Value])(body: TeXEnvironment => Unit) {
+    val env1 = env.enterGroup
+    cats foreach {
+      case (c, cat) => env1.category(c) = cat
+    }
+    body(env1)
+  }
+
+  def inputOf(env: TeXEnvironment, st: Stream[Char]): TeXLexerState =
+    TeXLexerState(st, StreamPosition(st, 0), ReadingState.N, env)
+
+  "a text stream" should "be parsed as a stream of character tokens" in {
+    val input = "this is a simple text".toStream
+    val expected = input.map { c =>
+      val cat = if(c == ' ') Category.SPACE else Category.LETTER
+      CharacterToken(c, cat)
+    }
+    stream(inputOf(env, input)) should be(expected)
+  }
+
+  "a control sequence" should "be recognized as such" in {
+    val input = "a \\test".toStream
+    val expected =
+      Stream(CharacterToken('a', Category.LETTER),
+        CharacterToken(' ', Category.SPACE),
+        ControlSequenceToken("test"))
+    stream(inputOf(env, input)) should be(expected)
+  }
+
+  it should "be recognized with single non letter character" in {
+    val input = "a \\'".toStream
+    val expected =
+      Stream(CharacterToken('a', Category.LETTER),
+        CharacterToken(' ', Category.SPACE),
+        ControlSequenceToken("'"))
+    stream(inputOf(env, input)) should be(expected)
+  }
+
+  "a special character in input stream" should "be transformed by preprocessor" in {
+    val input = "^^41".toStream
+    val expected = Stream(CharacterToken('A', Category.LETTER))
+    withCat(basicCategories) { env =>
+      stream(inputOf(env, input)) should be(expected)
+    }
+  }
+
+  it should "also be recognized when used with a letter" in {
+    val input = "^^r".toStream
+    val expected = Stream(CharacterToken('2', Category.OTHER_CHARACTER))
+    withCat(basicCategories) { env =>
+      stream(inputOf(env, input)) should be(expected)
+    }
+  }
+
+  it should "not be recognized if '^' has not category superscript" in {
+    val input = "^^41".toStream
+    val expected = Stream(
+      CharacterToken('^', Category.OTHER_CHARACTER),
+      CharacterToken('^', Category.OTHER_CHARACTER),
+      CharacterToken('4', Category.OTHER_CHARACTER),
+      CharacterToken('1', Category.OTHER_CHARACTER))
+    stream(inputOf(env, input)) should be(expected)
+  }
+
+}
